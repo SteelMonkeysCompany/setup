3831633291653c30e50896de2f7c386bd7da689b
arturovahatka@gmail.com <arturovahatka@gmail.com>
2020-04-13 19:15:39 +0300

    add centos 7 support add mail configuration use the const version of redash - 8.0.0.b32245

--- setup.sh
+++ setup.sh
@@ -1,22 +1,39 @@
 #!/usr/bin/env bash
-# This script setups dockerized Redash on Ubuntu 18.04.
+# This script setups dockerized Redash on CentOS 7 and Ubuntu 18.04.
 set -eu
 
 REDASH_BASE_PATH=/opt/redash
+COMPOSE_PATH=/usr/local/bin/docker-compose
+
+distro=unknown
 
 install_docker(){
-    # Install Docker
-    export DEBIAN_FRONTEND=noninteractive
-    sudo apt-get -qqy update
-    DEBIAN_FRONTEND=noninteractive sudo -E apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade 
-    sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common wget pwgen
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update && sudo apt-get -y install docker-ce
+    # Check for CentOS vs Ubuntu, then install Docker as appropriate
+    which yum >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        distro=centos7
+        #sudo yum -y update
+        sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+        sudo curl -L -o /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
+        sudo chown root:root /etc/yum.repos.d/docker-ce.repo
+    sudo restorecon -Fv /etc/yum.repos.d/docker-ce.repo
+        sudo yum -y install docker-ce pwgen yajl
+        sudo systemctl start docker
+    else
+        distro=ubuntu
+        sudo apt-get update
+        sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get update && sudo apt-get -y install docker-ce
+    fi
 
     # Install Docker Compose
-    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o ${COMPOSE_PATH}
+    sudo chmod +x ${COMPOSE_PATH}
+    if [ "${distro}" = "centos7" ]; then
+        sudo restorecon -Fv ${COMPOSE_PATH}
+    fi
 
     # Allow current user to run Docker commands
     sudo usermod -aG docker $USER
@@ -51,25 +68,43 @@ create_config() {
     echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
     echo "REDASH_SECRET_KEY=$SECRET_KEY" >> $REDASH_BASE_PATH/env
     echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
+    echo "REDASH_MAIL_SERVER=xxx" >> $REDASH_BASE_PATH/env
+    echo "REDASH_MAIL_PORT=xxx" >> $REDASH_BASE_PATH/env
+    echo "REDASH_MAIL_USERNAME=xxx" >> $REDASH_BASE_PATH/env
+    echo "REDASH_MAIL_PASSWORD=xxx" >> $REDASH_BASE_PATH/env
+    echo "REDASH_MAIL_DEFAULT_SENDER=xxx" >> $REDASH_BASE_PATH/env
 }
 
 setup_compose() {
+    # Determine newest release of Redash
     REQUESTED_CHANNEL=stable
-    LATEST_VERSION=`curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL"  | json_pp  | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
-
+    if [ "${distro}" = "centos7" ]; then
+        PRETTY_PRINTER=json_reformat
+		#TODO: add json parsing for centos7
+        #LATEST_VERSION=`curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL"  | ${PRETTY_PRINTER} | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
+        LATEST_VERSION='8.0.0.b32245'
+    else
+        PRETTY_PRINTER=json_pp
+        LATEST_VERSION=`curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL"  | ${PRETTY_PRINTER} | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
+    fi
+    # Update version string in the docker-compose yaml
     cd $REDASH_BASE_PATH
     GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-    wget https://raw.githubusercontent.com/getredash/setup/${GIT_BRANCH}/data/docker-compose.yml
+    curl -OL https://raw.githubusercontent.com/getredash/setup/${GIT_BRANCH}/data/docker-compose.yml
     sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
     echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
-    echo "export COMPOSE_FILE=/opt/redash/docker-compose.yml" >> ~/.profile
+    echo "export COMPOSE_FILE=${REDASH_BASE_PATH}/docker-compose.yml" >> ~/.profile
     export COMPOSE_PROJECT_NAME=redash
-    export COMPOSE_FILE=/opt/redash/docker-compose.yml
-    sudo docker-compose run --rm server create_db
-    sudo docker-compose up -d
+    export COMPOSE_FILE=${REDASH_BASE_PATH}/docker-compose.yml
+    # Start the Redash containers
+    sudo ${COMPOSE_PATH} run --rm server create_db
+    sudo ${COMPOSE_PATH} up -d
 }
 
 install_docker
 create_directories
 create_config
 setup_compose
+
+# Make the new docker user group effective, so the user doesn't need to re-login
+exec sg docker newgrp `id -gn`
